3e87762a0f6b11f8e87b40171af8e3520082bb7e
Johnny Lim <izeye@naver.com>
2018-02-24 16:22:09 +0900

    Initial commit

---
+++ src/main/java/com/izeye/test/servletinitializer/Application.java
@@ -0,0 +1,25 @@
+package com.izeye.test.servletinitializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Application.
+ *
+ * @author Johnny Lim
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
